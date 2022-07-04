51568c9dd85e5cd4ff73f879d9d77012209ff160
LudgeAnies <maks.xaskov12@gmail.com>
2022-07-04 22:08:17 +0300

    commit

---
+++ bot_mute.py
@@ -0,0 +1,35 @@
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+
+from database import Database
+import param
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=param.TOKEN)
+dp = Dispatcher(bot)
+db = Database('base.db')
+
+@dp.message_handler(commands=["mute"])
+async def mute_days_handler(message: types.Message):
+    if str(message.from_user.id) == param.ADMIN_ID:
+        if not message.reply_to_message:
+            await message.reply('Эта команда должна быть ответом на сообщение!')
+            return
+        mute_day = int(message.text[6:])
+        db.add_mute_days(message.reply_to_message.from_user.id, mute_day)
+        await message.bot.delete_message(param.CHAT_ID, message.message_id)
+        await message.reply_to_message.reply(f'Пользователь был замучен на {mute_day} дней!')
+
+
+@dp.message_handler()
+async def mess_handler(message: types.Message):
+    if not db.user_exists(message.from_user.id):
+        db.add_user(message.from_user.id)
+
+    if db.mute_user(message.from_user.id):
+        await message.delete()
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
+
---
+++ database.py
@@ -0,0 +1,47 @@
+import sqlite3
+import time
+
+
+class Database:
+    def __init__(self, db_file):
+        self.connection = sqlite3.connect(db_file)
+        self.cursor = self.connection.cursor()
+
+    def user_exists(self, user_id):
+        with self.connection:
+            result = self.cursor.execute("SELECT * FROM `users` WHERE `user_id` = ?", (user_id,)).fetchall()
+            return bool(len(result))
+
+    def add_user(self, user_id):
+        with self.connection:
+            return self.connection.execute("INSERT INTO `users` (`user_id`) VALUES (?)", (user_id,))
+
+    def mute_user(self, user_id):
+        with self.connection:
+            user = self.cursor.execute("SELECT * FROM `users` WHERE `user_id` = ?", (user_id,)).fetchone()
+            return int(user[2]) >= int(time.time())
+
+    def add_mute_days(self, user_id, mute_time):
+        with self.connection:
+            return self.connection.execute(
+                "UPDATE `users` SET `mute_time` = ? WHERE `user_id` = ?", (int(time.time()) + (
+                        mute_time * 60 * 60 * 24
+                ), user_id,)
+            ) # мут в днях
+
+    def add_mute_hours(self, user_id, mute_time):
+        with self.connection:
+            return self.connection.execute(
+                "UPDATE `users` SET `mute_time` = ? WHERE `user_id` = ?", (int(time.time()) + (
+                        mute_time * 60 * 60
+                ), user_id,)
+            ) # мут в часах
+
+    def add_mute_minutes(self, user_id, mute_time):
+        with self.connection:
+            return self.connection.execute(
+                "UPDATE `users` SET `mute_time` = ? WHERE `user_id` = ?", (int(time.time()) + (
+                        mute_time * 60
+                ), user_id,)
+            ) # мут в минутах
+        # 30:20
