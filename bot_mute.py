75823274600847063a2d8cd4b2eb35bf4eb10cf8
LudgeAnies <maks.xaskov12@gmail.com>
2022-07-05 20:55:44 +0300

    comit

--- bot_mute.py
+++ bot_mute.py
@@ -19,8 +19,20 @@ async def mute_days_handler(message: types.Message):
             return
         mute_day = int(message.text[6:])
         db.add_mute_days(message.reply_to_message.from_user.id, mute_day)
-        await message.bot.delete_message(param.CHAT_ID, message.message_id)
+        # await message.bot.delete_message(param.CHAT_ID, message.message_id)
         await message.reply_to_message.reply(f'Пользователь был замучен на {mute_day} дней!')
+
+@dp.message_handler(commands=["info"], commands_prefix='/')
+async def info_mute(message: types.Message):
+    if str(message.from_user.id) == param.ADMIN_ID:
+        if not message.reply_to_message:
+            await message.reply('Эта команда должна быть ответом на сообщение!')
+            return
+        if not db.mute_user(message.from_user.id):
+            await message.reply_to_message.reply('У пользователя отсутствует мут.')
+        if db.mute_user(message.from_user.id):
+            mute = db.mute_user(message.reply_to_message.from_user.id)
+            await message.reply_to_message.reply(f'Пользователь в муте на {mute} дней.')
 
 
 @dp.message_handler()
@@ -32,5 +44,5 @@ async def mess_handler(message: types.Message):
         await message.delete()
 
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    executor.start_polling(dp, skip_updates=True)
 
